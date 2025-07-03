5ffd2c28f23d3fc41acf884b4bf9ceb3c1d01b43
Maxene Januelle Salon <masalon@lab3r4s14.42abudhabi.ae>
2025-07-03 23:24:34 +0400

    is_valid_base.c

---
+++ is_valid_base/is_valid.base.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   is_valid.base.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: masalon <masalon@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/03 23:23:00 by masalon           #+#    #+#             */
+/*   Updated: 2025/07/03 23:23:29 by masalon          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	is_valid_base(char *base)
+{
+	int	i;
+	int	j;
+	int	len;
+
+	len = 0;
+	while (base[len])
+		len++;
+	if (len < 2)
+		return (0);
+	i = 0;
+	while (i < len)
+	{
+		if (base[i] == '+' || base[i] == '-' || base[i] < 32 || base[i] > 126)
+			return (0);
+		j = i + 1;
+		while (j < len)
+		{
+			if (base[i] == base[j])
+				return (0);
+			j++;
+		}
+		i++;
+	}
+	return (1);
+}
