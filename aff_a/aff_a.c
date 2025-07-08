0e63fc169e56feca2587e775fd8cd93baa6b1ac9
Maxene Januelle Salon <masalon@lab3r1s12.42abudhabi.ae>
2025-07-08 07:06:35 +0400

    added files

--- aff_a/aff_a.c
+++ aff_a/aff_a.c
@@ -2,21 +2,18 @@
 
 int	main(int argc, char **argv)
 {
-	int	i;
-	int	j;
+	int	i, j;
 
 	i = 1;
 	j = 0;
 	if (argc != 2)
-		write(1, "a", 2);
+		write(1, "a", 1);
 	else
 	{
 		while (argv[i][j])
 		{
 			if (argv[i][j] == 'a')
-			{
-				write (1, "a", 2);
-			}
+				write(1, "a", 1);
 			j++;
 		}
 		i++;
